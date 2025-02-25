c061281489ff00d08d74e31108d4081f40367467
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2025-02-25 13:05:04 -0600

    Feature: player turn switching and piece drawing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 	"image/color"
 	"log"
@@ -16,15 +17,39 @@ const (
 )
 
 var (
-	colorBlue   = color.RGBA{160, 206, 217, 255}
-	colorGreen  = color.RGBA{173, 247, 182, 255}
-	colorYellow = color.RGBA{255, 238, 147, 255}
-	board       [rows][columns]string
+	colorBlue     = color.RGBA{160, 206, 217, 255}
+	colorGreen    = color.RGBA{173, 247, 182, 255}
+	colorYellow   = color.RGBA{255, 238, 147, 255}
+	board         [rows][columns]string
+	currentPlayer = "X"
 )
 
 type Game struct{}
 
 func (g *Game) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.Key1) {
+		dropPiece(0, currentPlayer)
+		switchPlayer()
+	} else if inpututil.IsKeyJustPressed(ebiten.Key2) {
+		dropPiece(1, currentPlayer)
+		switchPlayer()
+	} else if inpututil.IsKeyJustPressed(ebiten.Key3) {
+		dropPiece(2, currentPlayer)
+		switchPlayer()
+	} else if inpututil.IsKeyJustPressed(ebiten.Key4) {
+		dropPiece(3, currentPlayer)
+		switchPlayer()
+	} else if inpututil.IsKeyJustPressed(ebiten.Key5) {
+		dropPiece(4, currentPlayer)
+		switchPlayer()
+	} else if inpututil.IsKeyJustPressed(ebiten.Key6) {
+		dropPiece(5, currentPlayer)
+		switchPlayer()
+	} else if inpututil.IsKeyJustPressed(ebiten.Key7) {
+		dropPiece(6, currentPlayer)
+		switchPlayer()
+	}
+
 	return nil
 }
 
@@ -38,12 +63,39 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 			vector.DrawFilledCircle(screen, float32(x+cellSize/2),
 				float32(y+cellSize/2), float32(cellSize/2)-5, color.White, true)
+
+			switch board[row][col] {
+			case "X":
+				vector.DrawFilledCircle(screen, float32(x+cellSize/2),
+					float32(y+cellSize/2), float32(cellSize/2)-10, colorGreen, true)
+			case "O":
+				vector.DrawFilledCircle(screen, float32(x+cellSize/2),
+					float32(y+cellSize/2), float32(cellSize/2)-10, colorYellow, true)
+			}
 		}
 	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
+}
+
+func dropPiece(pos int, value string) {
+	for row := rows - 1; row >= 0; row-- {
+		if board[row][pos] == "" {
+			board[row][pos] = value
+			break
+		}
+
+	}
+}
+
+func switchPlayer() {
+	if currentPlayer == "X" {
+		currentPlayer = "O"
+	} else {
+		currentPlayer = "X"
+	}
 }
 
 func main() {
